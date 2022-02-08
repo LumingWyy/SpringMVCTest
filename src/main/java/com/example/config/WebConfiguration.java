771918cfdbe47e22be8c7cc8947845107cb50644
wlm <599290100@qq.com>
2022-02-08 20:12:42 +0900

    @SessionScope

--- src/main/java/com/example/config/WebConfiguration.java
+++ src/main/java/com/example/config/WebConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -59,7 +60,7 @@ public class WebConfiguration implements WebMvcConfigurer {
     }
 
     @Bean
-    @RequestScope
+    @SessionScope
     public TestBean testBean(){
         return new TestBean();
     }
